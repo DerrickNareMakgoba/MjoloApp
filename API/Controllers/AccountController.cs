3207a778989e4ecd9ea5488b389c8fea31f152e3
makgobad <makgobad@invokesolutions.co.za>
2022-03-16 18:51:05 +0200

    added register endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
 using API.Data;
+using API.Dto;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
@@ -9,6 +12,21 @@ namespace API.Controllers
         public AccountController(DataContext context)
         {
             this._context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> RegisterAsync(UserDto _user)
+        {
+            var user = new AppUser {
+                UserName = _user.UserName,
+                Email = _user.Email
+            };
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+            
         }
     }
 }
