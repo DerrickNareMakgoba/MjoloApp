281d8572e03c04605342605fd1bf1f76ef4146fa
makgobad <makgobad@invokesolutions.co.za>
2022-03-16 19:17:42 +0200

    added method to return token

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,13 +1,43 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using API.Interfaces;
 using API.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration key)
+        {
+            this._key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key["TokenKey"]));
+        }
         public string createToken(AppUser user)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+
+            var cred = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokeDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(3),
+                SigningCredentials = cred
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokeDescriptor);
+
+            return tokenHandler.WriteToken(token);
+
         }
     }
 }
