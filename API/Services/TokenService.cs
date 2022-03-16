eedcbab7872de31f466244e0e001f88b1c5d47a4
makgobad <makgobad@invokesolutions.co.za>
2022-03-16 18:56:07 +0200

    added token service interface and class name

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,13 @@
+using API.Interfaces;
+using API.Models;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string createToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
